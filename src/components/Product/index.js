5d3e739b2954402e5d0338231d258eeb5540affb
Douglas Fanucchi <douglasfanucchi@gmail.com>
2020-08-16 09:57:49 -0300

    navigate to product page when product item is clicked

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ProductStyled from './styled';
+import { useNavigate } from 'react-router-dom';
 
 const Product = ({ data }) => {
+  const navigate = useNavigate();
+
   if (!data) return <></>;
-  console.log(data);
+
+  function handleClick(event) {
+    const { id } = data;
+    navigate(`/produto/${id}`);
+  }
+
   return (
-    <ProductStyled onClick={() => null}>
+    <ProductStyled onClick={handleClick}>
       <figure>
         <img src={data.fotos[0].src} />
         <figcaption>{data.nome}</figcaption>
