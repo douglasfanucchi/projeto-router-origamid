bd4e04cd22a67793d4adfcdfe0b881af4229606f
Douglas Fanucchi <douglasfanucchi@gmail.com>
2020-08-16 10:50:36 -0300

    Change document title when pages are changed

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -4,6 +4,7 @@ import productStyles from './Product.module.css';
 import Menu from '../components/Menu';
 import { useParams } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
+import { Helmet } from 'react-helmet';
 
 const Product = () => {
   const { id } = useParams();
@@ -16,25 +17,30 @@ const Product = () => {
   }, [id]);
 
   return (
-    <div className={pageStyles.container}>
-      <Menu />
-      {loading && <p style={{ marginTop: '60px' }}>Carregando...</p>}
+    <>
+      <Helmet>
+        <title>Products - {(data && data.nome) || ''}</title>
+      </Helmet>
+      <div className={pageStyles.container}>
+        <Menu />
+        {loading && <p style={{ marginTop: '60px' }}>Carregando...</p>}
 
-      {!loading && data && (
-        <section
-          className={`${productStyles.singleProduct} ${productStyles.active}`}
-        >
-          <figure>
-            <img src={data.fotos[0].src} />
-          </figure>
-          <div className={productStyles.productDescription}>
-            <h2>{data.nome}</h2>
-            <span className={productStyles.price}>R$ {data.preco}</span>
-            <p>{data.descricao}</p>
-          </div>
-        </section>
-      )}
-    </div>
+        {!loading && data && (
+          <section
+            className={`${productStyles.singleProduct} ${productStyles.active}`}
+          >
+            <figure>
+              <img src={data.fotos[0].src} />
+            </figure>
+            <div className={productStyles.productDescription}>
+              <h2>{data.nome}</h2>
+              <span className={productStyles.price}>R$ {data.preco}</span>
+              <p>{data.descricao}</p>
+            </div>
+          </section>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -4,6 +4,7 @@ import Menu from '../components/Menu/index';
 import useFetch from '../hooks/useFetch';
 import ProductsList from '../components/ProductsList/styled';
 import Product from '../components/Product';
+import { Helmet } from 'react-helmet';
 
 const Products = () => {
   const { request, data } = useFetch(
@@ -15,13 +16,18 @@ const Products = () => {
   }, []);
 
   return (
-    <div className={pageStyles.container}>
-      <Menu />
-      <ProductsList>
-        {data &&
-          data.map((product) => <Product key={product.id} data={product} />)}
-      </ProductsList>
-    </div>
+    <>
+      <Helmet>
+        <title>Products - List</title>
+      </Helmet>
+      <div className={pageStyles.container}>
+        <Menu />
+        <ProductsList>
+          {data &&
+            data.map((product) => <Product key={product.id} data={product} />)}
+        </ProductsList>
+      </div>
+    </>
   );
 };
 
